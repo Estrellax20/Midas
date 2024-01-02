5dc003081ff6139eb53b81cec21fbb76c098d629
Estrellax20 <anna.emma.bilska@gmail.com>
2024-01-02 17:53:24 +0100

    Haptic_motor_v2

--- haptic_motor.py
+++ haptic_motor.py
@@ -1,13 +1,13 @@
 from machine import I2C, Pin
 from time import sleep_ms
 
-# Adres I2C urządzenia Pololu 1638
+# I2C address of the Pololu 1638 device
 POLOLU_1638_ADDR = 0x5A
 
-# Inicjalizacja I2C
+# Initializing I2C
 i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)
 
-# Funkcja do ustawienia parametrów silnika wibracyjnego
+# Function to set parameters for the haptic motor
 def setup_haptic_motor():
     # Set mode to internal trigger input
     i2c.writeto(POLOLU_1638_ADDR, bytearray([0x01, 0x09]))
@@ -22,21 +22,21 @@ def setup_haptic_motor():
     status = i2c.readfrom(POLOLU_1638_ADDR, 1)
     print("Status:", status[0])
 
-# Funkcja do testowania silnika wibracyjnego
+# Function to test the haptic motor
 def vibrate_haptic_motor():
     seq = 0
     for wave in range(1, 124):
         i2c.writeto(POLOLU_1638_ADDR, bytearray([0x04 + seq, wave]))  # Set waveform
         i2c.writeto(POLOLU_1638_ADDR, bytearray([0x0C, 0x01]))  # Go command
-        sleep_ms(800)  # Give it enough time to play waveform effect
+        sleep_ms(800)
 
         if wave % 8 == 0:
             seq += 1
         if wave % 64 == 0:
             seq = 0
 
-# Inicjalizacja silnika wibracyjnego
+# Initializing the haptic motor
 setup_haptic_motor()
 
-# Testowanie silnika wibracyjnego
+# Testing the haptic motor
 vibrate_haptic_motor()
